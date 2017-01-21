e7d16a32abcd789f40d2643c7f38bae552abe240
Matthew Tse <mtse9779@gmail.com>
2017-01-21 06:24:09 -0500

    Add batch saving endpoint Takes a list of b64 strings and save them after converting them to np arrays

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template, send_from_directory, abort
-from img_processing import batch_convert, image_data2array
+from img_processing import batch_convert, image_data2array, store_array_list
 app = Flask(__name__)
 
 
@@ -10,7 +10,8 @@ def webcam():
 
 @app.route('/convert', methods=['POST'])
 def convert():
-    if not request.json or 'uid' not in request.json or 'frame' not in request.json or 'data' not in request.json:
+    expected_values = ['uid','frame','data']
+    if not request.json or not all(value in request.json for value in expected_values):
         abort(400)
     data2convert = (
         request.json['uid'],
@@ -18,8 +19,32 @@ def convert():
         request.json['data'],
     )
     image_array = image_data2array(*data2convert)
+    if 'save' in request.json:
+        print 'saving......'
+        if request.json['save']:
+            classification = request.json['class']
+            try:
+                store_array_list(image_array,classification)
+            except Exception as e:
+                print e
+                abort(500)
+
     return jsonify({"success":True})
-    # # return jsonify(getDemographic(request.json['video_id']))
+
+@app.route('/batch_save', methods=['POST'])
+def batch_save():
+    if not request.json or 'data' not in request.json:
+        abort(400)
+    image_array_list = batch_convert(request.json['data'])
+    if 'class' in request.json:
+        classification = request.json['class']
+        try:
+            store_array_list(image_array_list, classification + str(request.json['data'][0]['uid']))
+        except Exception as e:
+            print 'Error:' + e
+            abort(500)
+    return jsonify({"success":True})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- img_processing.py
+++ img_processing.py
@@ -1,6 +1,7 @@
 import base64
 import numpy
 import hickle
+import pickle
 from PIL import Image
 from io import BytesIO
 
@@ -17,16 +18,28 @@ def image_data2array(uid, frame, b64data):
 def batch_convert(inc_data_list):
     data_list = []
     for data in inc_data_list:
-        data_list.append(image_data2array(*data))
+        data2convert = (
+            data['uid'],
+            data['frame'],
+            data['data'],
+        )
+        data_list.append(image_data2array(*data2convert))
     return data_list
 
-def store_array(array_list,filename):
+def store_b64_str(data,filename):
     with open(filename, 'w+') as f:
-        hickle.dump(array_list, filename+'.hkl')
+        pickle.dump(data, './data/b64/'+filename+'.pkl')
 
-def retrieve_array(filename):
+def store_array_list(array_list,filename):
+    with open(filename, 'w+') as f:
+        hickle.dump(array_list, './data/'+filename+'.hkl')
+
+def retrieve_array_list(filename):
     with open(filename) as f:
-        array_list = hickle.load(filename)
+        array_list = hickle.load('./data/'+filename+'.hkl')
+    return array_list
+
+
 # ######turn b64 string into jpg and save
 # filename = 'example_b64_str_img.txt'
 # with open(filename) as f:
