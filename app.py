81c075b05da1bb0f6177cd86000e38b5d54ddd6e
Matthew Tse <mtse9779@gmail.com>
2017-01-21 05:15:08 -0500

    Add Convert endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, render_template, send_from_directory, abort
-
+from img_processing import batch_convert, image_data2array
 app = Flask(__name__)
 
 
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def webcam():
     return render_template("webcam.html")
 
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    if not request.json or 'uid' not in request.json or 'frame' not in request.json or 'data' not in request.json:
+        abort(400)
+    data2convert = (
+        request.json['uid'],
+        request.json['frame'],
+        request.json['data'],
+    )
+    image_array = image_data2array(*data2convert)
+    return jsonify({"success":True})
+    # # return jsonify(getDemographic(request.json['video_id']))
 
 if __name__ == "__main__":
     app.run(debug=True)
