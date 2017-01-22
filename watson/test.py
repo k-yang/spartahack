436f469da0e9bdf4129cf37b6c95ce91e32e3679
Kevin Yang <k46yang@uwaterloo.ca>
2017-01-21 23:30:56 -0500

    Added ui changes

---
+++ watson/test.py
@@ -0,0 +1,3 @@
+from visual_recognition import *
+
+print classify_image('00665173-78c5-4b63-9f54-747e22cd3328.png')
--- watson/visual_recognition.py
+++ watson/visual_recognition.py
@@ -3,30 +3,62 @@ import json
 from os.path import join, dirname
 
 visual_recognition = VisualRecognitionV3('2016-05-20', api_key='3e5cc32fd8601944b7d1bf2e0fa69813e96be789')
-
-print(json.dumps(visual_recognition.list_classifiers(), indent=2))
-
-# print(json.dumps(visual_recognition.delete_classifier('SpartaHacks_1858792783')))
-
-# with open(join(dirname(__file__), '../image_data/random/Archive.zip'), 'rb') as random_data, \
-#         open(join(dirname(__file__), '../image_data/apple/Archive.zip'), 'rb') as apple_data, \
-#         open(join(dirname(__file__), '../image_data/pizza/Archive.zip'), 'rb') as pizza_data, \
-#         open(join(dirname(__file__), '../image_data/hamburger/Archive.zip'), 'rb') as hamburger_data:
-#     print(json.dumps(visual_recognition.create_classifier('SpartaHacks',
-#                                                           random_positive_examples=random_data,
-#                                                           apple_positive_examples=apple_data,
-#                                                           hamburger_positive_examples=hamburger_data,
-#                                                           pizza_positive_examples=pizza_data),
-#                      indent=2))
-#
-# print(json.dumps(visual_recognition.list_classifiers(), indent=2))
+CLASSIFIER_ID = 'SpartaHacks_111614200'
 
 
-with open(join(dirname(__file__), '../image_data/pizza/04df0a57-630f-4be2-a566-ac33b9633e7d.png'), 'rb') as image:
-    print(json.dumps(visual_recognition.classify(image, classifier_ids=['SpartaHacks_1703170307']), indent=2))
-#
-# with open(join(dirname(__file__), '../image_data/hamburger/0cbfbc1d-d1a2-44a6-8d55-b4ac117eb712.png'), 'rb') as image:
-#     print(json.dumps(visual_recognition.classify(image, classifier_ids=['SpartaHacks_1703170307']), indent=2))
-#
-# with open(join(dirname(__file__), '../image_data/pizza/1eccf6e5-e0ed-4387-b756-07040f20f557.png'), 'rb') as image:
-#     print(json.dumps(visual_recognition.classify(image, classifier_ids=['SpartaHacks_1703170307']), indent=2))
+def delete_classifier():
+    print(json.dumps(visual_recognition.delete_classifier(CLASSIFIER_ID)))
+
+
+def list_all_classifiers():
+    print(json.dumps(visual_recognition.list_classifiers(), indent=2))
+
+
+def list_classifier():
+    print(json.dumps(visual_recognition.get_classifier(CLASSIFIER_ID), indent=2))
+
+
+def create_classifier():
+    with open(join(dirname(__file__), '../test_data/bowl/Archive.zip'), 'rb') as bowl_data, \
+            open(join(dirname(__file__), '../test_data/apple/Archive.zip'), 'rb') as apple_data, \
+            open(join(dirname(__file__), '../test_data/orange/Archive.zip'), 'rb') as orange_data, \
+            open(join(dirname(__file__), '../test_data/pasta/Archive.zip'), 'rb') as pasta_data, \
+            open(join(dirname(__file__), '../test_data/soup/Archive.zip'), 'rb') as soup_data, \
+            open(join(dirname(__file__), '../test_data/help.zip'), 'rb') as help_data:
+        print(json.dumps(visual_recognition.create_classifier('SpartaHacks',
+                                                              bowl_positive_examples=bowl_data,
+                                                              apple_positive_examples=apple_data,
+                                                              orange_positive_examples=orange_data,
+                                                              pasta_positive_examples=pasta_data,
+                                                              soup_positive_examples=soup_data,
+                                                              help_positive_examples=help_data),
+                         indent=2))
+
+
+def classify_image(filename):
+    intents = {}
+    with open(join(dirname(__file__), '../image_data/{}'.format(filename)), 'rb') as data:
+        result = visual_recognition.classify(data, classifier_ids=[CLASSIFIER_ID])
+        for image in result['images']:
+            if len(image['classifiers']) > 0:
+                for _class in image['classifiers'][0]['classes']:
+                    if _class['class'] not in intents:
+                        intents[_class['class']] = _class['score']
+                    else:
+                        intents[_class['class']] += _class['score']
+        print(json.dumps(result, indent=2))
+
+    highest_score = 0
+    intent = None
+    for key in intents:
+        if intents[key] > highest_score:
+            highest_score = intents[key]
+            intent = key
+
+    return intent
+
+# list_all_classifiers()
+# delete_classifier()
+# create_classifier()
+# list_classifier()
+# classify_image('bowltest.png')
